6f393d7a5f0226c7142318eb5eadaaadbfca44d1
iss1977 <ioan.szucs@gmail.com>
2020-09-19 13:54:43 +0200

    Deleted rejected rej file

--- src/Controller.java
+++ src/Controller.java
@@ -19,37 +19,15 @@ public class Controller {
         return utopiaTime;
     }
 
-<<<<<<< HEAD
-    private long lastUpdateAtSecond = 0; // only to see if a second is passed or not since last tick. Only on second change will be a tick on utopia generated
-    Calendar calendar = Calendar.getInstance(); // will also be used to store the time when utopia was started. Search for other posibilities
-=======
     private long lastUpdateAtSecond=0; // only to see if a second is passed or not since last tick. Only on second change will be a tick on utopia generated
     Calendar calendar = Calendar.getInstance(); // will also be used to store the time when utopia was started. Search for other possibilities
 
     public Tester tester=new Tester();
->>>>>>> Develop
 
     Controller() { // CONSTRUCTOR
         this.startedAT = new Date();
         calendar.set(2100, Calendar.JANUARY, 1, 0, 0, 0);
         this.utopiaTime = calendar.getTime();
-<<<<<<< HEAD
-    }
-
-    Controller(Date startDateTime) { // CONSTRUCTOR
-
-    } // end of constructor
-
-    // ----------------------------------|  T h e   T I C K |---------------------------------------\\
-    public void tick() {
-        if (this.lastUpdateAtSecond == 10) GlobalStacker.stopUtopia();
-        if (this.lastUpdateAtSecond == 3) {
-            this.doActivity(GlobalStacker.registeredActivities.get(1), Main.myCitizen);
-        }
-
-        // let's tick every citizen
-        for (Citizen citizen : GlobalStacker.registredCitizens) {
-=======
         System.out.println("myController (this) class is "+this.getClass().getName());
     } // end of constructor
 
@@ -59,40 +37,22 @@ public class Controller {
 
         // let's tick every citizen and activity
         for (Citizen citizen: GlobalStacker.registredCitizens) {
->>>>>>> Develop
             citizen.ontick();
         }
 
         if (this.lastUpdateAtSecond % Tester.runTheTestOnEveryThisNumberOfTicks == 0) tester.runDeveloperTest(); // here can we put some test code.
 
-<<<<<<< HEAD
-    } //  end of tick()
-=======
         
 
 
         if (this.lastUpdateAtSecond==2) GlobalStacker.stopUtopia();
     } //  end of tick()  -----------------------------------------------------------------------------\\
->>>>>>> Develop
 
 
 
 
     public boolean cycle() { // this can be run as many times as wished. If a complete second is passed since last tick(), then a new tick() is called
         // on each tick we must update utopiaTime !
-<<<<<<< HEAD
-        // one step has 900 utopia seconds = 15min.
-        final int ONEEARTHSECONDIS = 900;
-        long diffInMillies = Math.abs(new Date().getTime() - startedAT.getTime());
-        long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS); // represents the seconds between start of the program and now.
-        if (diff != this.lastUpdateAtSecond) {
-            this.lastUpdateAtSecond = diff;
-
-            this.utopiaTime = GlobalStacker.addSecondsToJavaUtilDate(calendar.getTime(), (int) (diff * GlobalStacker.oneSecondOnEarthEqualsThisManySecondsOnUtopia));
-            this.utopiaTime = addSecondsToJavaUtilDate(calendar.getTime(), (int) (diff * ONEEARTHSECONDIS));
-            System.out.println("Calling a new tick! Utopia is running since:" + diff + " earth seconds. Time on Utopia is now:" + this.utopiaTime.toString());
-            System.out.println("\r\nCalling a new tick! Utopia is running since:" + diff + " earth seconds. Time on Utopia is now:" + this.utopiaTime.toString());
-=======
         // one step has 3600 utopia seconds = 1 hour. You can change this in class GlobalStacker
 
         long diffInMilliesInWorldClock = Math.abs(new Date().getTime() - startedAT.getTime());
@@ -103,7 +63,6 @@ public class Controller {
         if (diff!=this.lastUpdateAtSecond){
             this.lastUpdateAtSecond=diff;
             System.out.println("\r\nCalling a new tick! Utopia is running since:"+diff+" earth seconds. Time on Utopia is now:"+this.utopiaTime.toString());
->>>>>>> Develop
             this.tick(); // here happens everything.
         }
         return GlobalStacker.utopiaIsRunning;
@@ -111,14 +70,8 @@ public class Controller {
     } // end of cycle()
 
     //every activity that is created must be registered here to be able to make an offer to the citizen.
-<<<<<<< HEAD
-    public Integer registerActivity(ActivityBlueprint activity) { // the activity will be stored in GlobalStacker \\
-        GlobalStacker.registeredActivities.put(++GlobalStacker.numberOfRegisteredActivities, activity);
-        return GlobalStacker.numberOfRegisteredActivities;
-=======
     public void registerActivity(ActivityBlueprint activity){ // the activity will be stored in GlobalStacker \\
         GlobalStacker.registeredActivities.add(activity);
->>>>>>> Develop
     }
 
     // call this method to register a citizen (add it to ArrayList GlobalStacker.registredCitizens )
@@ -144,26 +97,6 @@ public class Controller {
     }
 
 
-<<<<<<< HEAD
-    public void doActivity(ActivityBlueprint activity, Citizen citizen) {
-        System.out.println("The activity " + activity.category + " hat returned an offer of " + activity.getOffer(citizen) + " points.");
-        System.out.println("The citizen " + citizen.name + "," + citizen.age + " old is going to do the activity " + activity.category + " because he knows, " + activity.serviceDescription);
-        int hunger = Benefits.hunger;
-    }
-
-
-
-
-    private Date addSecondsToJavaUtilDate(Date date, int seconds) { // used to add seconds to a date. => utopiaTime
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.SECOND, seconds);
-        return calendar.getTime();
-    }
-
-
-}
-=======
     public boolean doActivity(ActivityBlueprint activity, Citizen citizen){
         if (activity==null || citizen==null) {
             System.out.println("!!!!!!!!!!!!!!!!!!!   doActivity(ActivityBlueprint activity, Citizen citizen) in Controller :  NULL reference as parameter. Exiting the method");
@@ -182,4 +115,3 @@ public class Controller {
     }
 
   } // end of class
->>>>>>> Develop
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
 public class Main {
 
-<<<<<<< HEAD
-
-    public static Controller myController = new Controller(); // controller must run before the first citizen is created.
-    public static Citizen myCitizen = new Citizen("Donald Trump", 2030, 5, 25);
-
-    public static ActivityFood myFoodService = new ActivityFood();
-
-
-    public static void main(String[] args) {
-
-        System.out.println("My Citizen is alive. He is " + myCitizen.toString());
-        myController.registerCitizen(myCitizen); // register the static citizen
-
-        myController.registerCitizen(new Citizen("Justin Bieber", 2072, 6, 22)); // register the second citizen can work like that.
-        myController.registerCitizen(new Citizen("Leonardo DiCaprio", 2058, 2, 17));
-        myController.registerCitizen(new Citizen("Kim Kardashian", 2068, 1, 11));
-        myController.registerCitizen (new Citizen("Bill Gates", 2037, 9, 2)); // 5 in total
-=======
     public static Controller myController = new Controller();;
 
     public static void main(String[] args) {
@@ -33,15 +15,9 @@ public class Main {
         myController.registerCitizen(new Citizen("Kim Kardashian", 2068,1,11));
         myController.registerCitizen(new Citizen("Bill Gates", 2037,9,2)); // 5 in total
         myController.generateCitizens(15);
->>>>>>> Develop
 
         System.out.println("Controller is alive since " + myController.getStartedAT().toString());
 
-<<<<<<< HEAD
-        int controllNumber = myController.registerActivity(myFoodService);
-        System.out.println("Activity registered. Category:" + myFoodService.category + " | Description:" + myFoodService.serviceDescription + " | Registration nr: " + controllNumber + " | Created by:" + myFoodService.createdBy);
-=======
->>>>>>> Develop
 
 
         // this is an endless loop. It will run so long as the controller returns true. It will return false on the next tick() after we call GlobalStacker.stopUtopia()
@@ -50,14 +26,6 @@ public class Main {
         while (endlessloop) {
             endlessloop = myController.cycle();
             //System.out.println("My citizen is "+myCitizen.getAgeInDays());
-<<<<<<< HEAD
-
-
-        }
-    }
-
-}
-=======
         }// end of endless loop
     } // end of psvm
 
@@ -66,4 +34,3 @@ public class Main {
     }
 
 }//end of class
->>>>>>> Develop
